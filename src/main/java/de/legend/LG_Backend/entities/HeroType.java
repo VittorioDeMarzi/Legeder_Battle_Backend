7dd19a8224e3c5775e94225927d969ec2aa3a478
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-10 14:37:08 +0200

    add heroType with getter and setter

--- src/main/java/de/legend/LG_Backend/entities/HeroType.java
+++ src/main/java/de/legend/LG_Backend/entities/HeroType.java
@@ -1,9 +1,6 @@
 package de.legend.LG_Backend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class HeroType {
@@ -11,5 +8,47 @@ public class HeroType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private int minPower;
+
+    @Column
+    private int maxPower;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinPower() {
+        return minPower;
+    }
+
+    public void setMinPower(int minPower) {
+        this.minPower = minPower;
+    }
+
+    public int getMaxPower() {
+        return maxPower;
+    }
+
+    public void setMaxPower(int maxPower) {
+        this.maxPower = maxPower;
+    }
 
 }
