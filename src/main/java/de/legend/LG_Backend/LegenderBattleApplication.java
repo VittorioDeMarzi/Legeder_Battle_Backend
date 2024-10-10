f257abec018f40f4631d3f1846b994a6bf6533c0
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-10-10 11:01:40 +0200

    first commit

---
+++ src/main/java/de/legend/LG_Backend/LegenderBattleApplication.java
@@ -0,0 +1,13 @@
+package de.legend.LG_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegenderBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegenderBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/legend/LG_Backend/LegenderBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.legend.LG_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegenderBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
