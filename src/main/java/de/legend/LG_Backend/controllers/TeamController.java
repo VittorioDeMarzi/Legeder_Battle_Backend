e4f3b270e4f5a007bae2f3f1d51fbe13133e383e
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-11 16:20:47 +0200

    update Exceptions

--- src/main/java/de/legend/LG_Backend/controllers/TeamController.java
+++ src/main/java/de/legend/LG_Backend/controllers/TeamController.java
@@ -3,10 +3,14 @@ package de.legend.LG_Backend.controllers;
 import de.legend.LG_Backend.dtos.TeamDtos.TeamRequestDto;
 import de.legend.LG_Backend.dtos.TeamDtos.TeamResponseDto;
 import de.legend.LG_Backend.servicies.TeamService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("api/v1/legender_battle/team")
@@ -19,18 +23,23 @@ public class TeamController {
     }
 
     @PostMapping()
-    public ResponseEntity<Void> newTeam(@RequestBody @Validated TeamRequestDto dto, Authentication authentication){
+    public ResponseEntity<Void> newTeam(@RequestBody @Validated TeamRequestDto dto, Authentication authentication) {
         teamService.addNewTeam(dto, authentication);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping()
-    public ResponseEntity<TeamResponseDto> getTeamData(@RequestBody Authentication authentication){
-        return ResponseEntity.ok(teamService.getTeamData(authentication));
+    public ResponseEntity<TeamResponseDto> getTeamData(Authentication authentication) {
+        try {
+            TeamResponseDto teamData = teamService.getTeamData(authentication);
+            return ResponseEntity.ok(teamData);
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @DeleteMapping
-    public void deleteTeam(Authentication authentication){
+    public void deleteTeam(Authentication authentication) {
         try {
             teamService.deleteTeam(authentication);
         } catch (Exception e) {
@@ -39,7 +48,7 @@ public class TeamController {
     }
 
     @PutMapping
-    public void updateTeamName(TeamRequestDto dto, Authentication authentication){
+    public void updateTeamName(TeamRequestDto dto, Authentication authentication) {
         try {
             teamService.updateTeamName(dto, authentication);
         } catch (Exception e) {
