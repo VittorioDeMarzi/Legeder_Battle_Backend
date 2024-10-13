ca67cdf9c79674cbcbd23df82458564ec98af4c1
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-13 11:01:08 +0200

    comment query

--- src/main/java/de/legend/LG_Backend/repository/TeamRepository.java
+++ src/main/java/de/legend/LG_Backend/repository/TeamRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    List<Hero> findHeroesByTeamAndIsTakenTrue(Team team);
+//    List<Hero> findByTeamIdAndIsTakenTrue(Long teamId);
 }
